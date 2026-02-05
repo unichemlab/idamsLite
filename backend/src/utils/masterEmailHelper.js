3775a60ed50dfd413d2859ec54f1973fda0b18f3
runisingh26 <nishantsingh101991@gmail.com>
2026-02-05 18:20:52 +0530

    admin approval link

--- backend/src/utils/masterEmailHelper.js
+++ backend/src/utils/masterEmailHelper.js
@@ -49,7 +49,7 @@ async function sendApprovalEmail({
     }[action] || action;
 
     const appUrl = process.env.REACT_APP_API_URL || "http://localhost:3000";
-    const approvalUrl = `${appUrl}/admin-approvals/${approvalId}`;
+    const approvalUrl = `${appUrl}/admin-approval/${approvalId}`;
 
     // Format record data for email
     const recordSummary = recordData
